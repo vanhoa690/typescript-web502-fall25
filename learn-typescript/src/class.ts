f9f4e96403cba8f5ec48d8ceb8c514f961b85efd
vanhoa690 <vanhoa690@gmail.com>
2025-09-25 19:13:37 +0700

    extends

--- learn-typescript/src/class.ts
+++ learn-typescript/src/class.ts
@@ -1,15 +1,46 @@
 class Person {
-  name: string;
-  age: number;
+  public name: string; // đọc và ghi ở trong hoặc ngoài class
+  protected gender: boolean; // chỉ đọc và ghi ở trong class và class kế thừa
+  private age: number; // chỉ đọc và ghi ở trong chính class đó
+  readonly point: number;
 
-  constructor(name: string, age: number) {
+  constructor(name: string, age: number, gender: boolean, point: number) {
     this.name = name;
     this.age = age;
+    this.gender = gender;
+    this.point = point;
   }
 
-  say() {
-    return "hello";
+  say(): string {
+    return `hello ${this.name}`;
   }
 }
 
-const p1 = new Person("hoadv", 36);
+const p1 = new Person("hoadv", 36, true, 10);
+const result = p1.say();
+console.log(result);
+console.log(p1.name);
+
+// Ke thua: extends
+class User extends Person {
+  public role: string;
+  constructor(
+    name: string,
+    age: number,
+    gender: boolean,
+    point: number,
+    role: string
+  ) {
+    super(name, age, gender, point); // super goi tu cha
+    this.role = role;
+  }
+
+  //override: ghi de method cua cha
+  say(): string {
+    return `xin chao ${this.name}`;
+  }
+}
+
+const u1 = new User("hoadv", 36, true, 10, "admin");
+u1.name;
+console.log(u1.say());
