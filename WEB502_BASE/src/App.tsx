951d111264626f0f182c0db4e1c371093e43e535
vanhoa690 <vanhoa690@gmail.com>
2025-09-30 19:00:28 +0700

    components button

--- WEB502_BASE/src/App.tsx
+++ WEB502_BASE/src/App.tsx
@@ -1,9 +1,16 @@
 import { Toaster } from "react-hot-toast";
+import Button from "./components/Button";
 
 function App() {
+  const handleClick = (name: string) => alert("Clicked!" + name);
+  const handleClick2 = () => alert("Clicked!");
+
   return (
     <>
       <div>Hello React Typesscript</div>
+      <Button label="Click Me 1" onClick={() => handleClick("hoadv")} />
+      <Button label="Click Me 21" onClick={handleClick2} />
+      <Button label="Click Me 3" onClick={handleClick2} />
       <Toaster />
     </>
   );
