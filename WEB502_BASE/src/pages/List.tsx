76466aaa167a56dae43c873fa90daeb1e4e75786
vanhoa690 <vanhoa690@gmail.com>
2025-10-11 19:07:18 +0700

    list + delete

--- WEB502_BASE/src/pages/List.tsx
+++ WEB502_BASE/src/pages/List.tsx
@@ -1,34 +1,67 @@
+import axios, { AxiosError } from "axios";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+interface IProduct {
+  id: number;
+  name: string;
+  image: string;
+  price: number;
+  description: string;
+  category: string;
+}
 function List() {
+  const [products, setProducts] = useState<IProduct[]>([]);
+  useEffect(() => {
+    const getProducts = async () => {
+      const res = await axios.get("http://localhost:3000/products");
+      setProducts(res.data);
+    };
+    getProducts();
+  }, []);
+
+  const handleDelete = async (id: number) => {
+    try {
+      if (window.confirm("Muon xoa hay ko")) {
+        await axios.delete("http://localhost:3000/products/" + id);
+        setProducts((productsPrev) => {
+          return productsPrev.filter((item) => item.id !== id);
+        });
+        toast.success("Xoa thanh cong");
+      }
+    } catch (error) {
+      toast.error(`Error: ${(error as AxiosError)?.message}`);
+    }
+  };
   return (
     <div>
       <h1>Danh sách</h1>
       <table className="table table-hover table-bordered table-striped">
         <thead>
           <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Image</th>
+            <th scope="col">Price</th>
+            <th scope="col">Actions</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colSpan={2}>Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          {products.map((item, index) => {
+            return (
+              <tr key={index}>
+                <th scope="row">{item.id}</th>
+                <td>{item.name}</td>
+                <td>
+                  <img src={item.image} width={100} />
+                </td>
+                <td>{item.price}</td>
+                <td>
+                  <button onClick={() => handleDelete(item.id)}>Delete</button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
