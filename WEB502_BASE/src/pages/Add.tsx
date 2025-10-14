ed03e899558406bb9871cfc4bd1cefff6394999c
vanhoa690 <vanhoa690@gmail.com>
2025-10-14 19:02:42 +0700

    submit form

--- WEB502_BASE/src/pages/Add.tsx
+++ WEB502_BASE/src/pages/Add.tsx
@@ -1,16 +1,20 @@
 import axios from "axios";
 import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 
 interface FormData {
   name: string;
+  image: string;
 }
 function Add() {
   // react hook form
   const { register, handleSubmit } = useForm<FormData>();
 
-  const onSubmit = (values: FormData) => {
+  const onSubmit = async (values: FormData) => {
     console.log("submit", values);
+    await axios.post("http://localhost:3000/products", values);
+    toast.success("them thanh cong");
   };
 
   return (
@@ -29,7 +33,18 @@ function Add() {
           />
           {/* {errors} */}
         </div>
-
+        <div className="mb-3">
+          <label htmlFor="image" className="form-label">
+            Image
+          </label>
+          <input
+            {...register("image")} // value, onChange ....
+            type="text"
+            className="form-control"
+            id="image"
+          />
+          {/* {errors} */}
+        </div>
         <button type="submit" className="btn btn-primary">
           Submit
         </button>
